ebf87ee4004e102f03bde5023b403cd50a24d79f
plmokn5w <plmokn5w@naver.com>
2022-12-06 18:00:59 +0900

    weapon, bullet class implement

--- project/hyeonsu/game/game.py
+++ project/hyeonsu/game/game.py
@@ -14,6 +14,7 @@ running = True
 
 background = Object((screen_width / 2, screen_height / 2), "background")
 player = Player("character_scientist", 100, 6)
+player.weapons.append(Weapon(player, "weapon_cpp", 0.1, 10, 60))
 
 for _ in range(5):
     amount = random.randint(150, 200)
@@ -23,6 +24,8 @@ for _ in range(5):
     dir = random.choice([-1, 1])
     dy = amount * dir
     Monster((player.x + dx, player.y + dy), "zero_division", player)
+
+exp_bar = EXPBar(player, "exp_bar")
 
 # 게임
 while running:
@@ -52,6 +55,17 @@ while running:
         
     player.move()
 
+    for weapon in player.weapons:
+        weapon.shotBullet()
+    
+    for bullet in player_bullet_list:
+        bullet.move()
+        for monster in monster_list:
+            if bullet.collide(monster):
+                monster.die()
+                player_bullet_list.remove(bullet)
+                break
+
     for monster in monster_list:
         monster.move()
         if monster.collide(player):
@@ -62,18 +76,30 @@ while running:
     for exp in exp_list:
         exp.move()
         if exp.collide(player):
-            player.gain_exp(exp)
+            player.gainEXP(exp)
+    
+    exp_bar.resize()
+
+    w_x = - (player.x + player.width / 2) + screen_width / 2
+    w_y = - (player.y + player.height / 2) + screen_height / 2
     
     screen.blit(background.img, (background.x, background.y))
-    screen.blit(player.img, (player.x, player.y))
+    screen.blit(player.img, (player.x + w_x, player.y + w_y))
+    for bullet in player_bullet_list:
+        screen.blit(bullet.img, (bullet.x + w_x, bullet.y + w_y))
     for monster in monster_list:
-        screen.blit(monster.img, (monster.x, monster.y))
+        screen.blit(monster.img, (monster.x + w_x, monster.y + w_y))
     for exp in exp_list:
-        screen.blit(exp.img, (exp.x, exp.y))
+        screen.blit(exp.img, (exp.x + w_x, exp.y + w_y))
+    screen.blit(exp_bar.img, (0, 0))
 
     game_font = pygame.font.Font(None, 40)
-    weapon_left_text = game_font.render("Level : {}".format(player.lv), True, (255, 255, 235))
-    screen.blit(weapon_left_text, (10, 10))
+    lv_text = game_font.render("LV : {}".format(player.lv), True, (255, 255, 235))
+    screen.blit(lv_text, (10, 60))
+
+    game_font = pygame.font.Font(None, 20)
+    exp_text = game_font.render("exp : {}".format(player.exp), True, (255, 255, 235))
+    screen.blit(exp_text, (10, 90))
 
     # 화면 업데이트
     pygame.display.update()
--- project/hyeonsu/game/library.py
+++ project/hyeonsu/game/library.py
@@ -49,6 +49,7 @@ class Player(Object):
                  initial_hp, initial_speed):
         super().__init__((screen_width / 2, screen_height / 2), character_name)
         self.lv = 0
+        self.exp = 0
         self.max_hp = initial_hp
         self.hp = self.max_hp
         self.speed = initial_speed
@@ -59,9 +60,15 @@ class Player(Object):
     def move(self):
         super().move(((self.to_x, self.to_y)))
     
-    def gain_exp(self, exp):
-        self.lv += exp.exp_amount
+    def gainEXP(self, exp):
+        self.exp += exp.exp_amount
         exp_list.remove(exp)
+        if self.exp >= 5:
+            self.exp = 0
+            self.levelUp()
+    
+    def levelUp(self):
+        self.lv += 1
 
 # 몬스터
 monster_name_list = ["zero_division", "stack_overflow", "range_out_head"]
@@ -108,24 +115,100 @@ class Monster(Object):
 # 경험치
 class EXP(Object):
     def __init__(self, monster, path):
-        super().__init__((monster.x, monster.y), path)
+        super().__init__((monster.x + monster.width / 2, monster.y + monster.height / 2), path)
         self.player = monster.player
         self.exp_amount = monster.max_hp
-        self.speed = 10
+        self.speed = 7
         exp_list.append(self)
     
     def move(self):
         vec_x = self.player.x - self.x
         vec_y = self.player.y - self.y
         side = math.sqrt(vec_y ** 2 + vec_x ** 2)
-        if side <= 100:
+        if side <= 300:
             super().move(
                 (vec_x / side * self.speed, vec_y / side * self.speed))
 
+
+# 경험치 바
+class EXPBar():
+    def __init__(self, player, path):
+        self.player = player
+        self.img = pygame.image.load(os.path.join(image_path, path + ".png"))
+        self.img = pygame.transform.scale(self.img, (1, 50))
+        self.rect = self.img.get_rect()
+        self.rect.left = 0
+        self.rect.top = 0
+
+    def resize(self):
+        self.img = pygame.transform.scale(self.img, (int(screen_width * (self.player.exp / 5)) + 1, 50))
+        self.rect = self.img.get_rect()
+        self.rect.left = 0
+        self.rect.top = 0
+
 class Weapon:
-    def __init__(self):
-        pass
+    def __init__(self, player, path, speed, dmg, atk_speed):
+        self.lv = 1
+        self.bullet = path
+        self.bullet_speed = speed
+        self.bullet_dmg = dmg
+        self.attack_delay = atk_speed
+        self.cnt = 0
+        self.player = player
+        player.weapons.append(self)
+    
+    def atk(self):
+        if self.cnt == self.attack_delay:
+            self.cnt = 0
+            return True
+        else:
+            self.cnt += 1
+            return False
+
+    def shotBullet(self):
+        if not self.atk():
+            return
+        p_x = self.player.x
+        p_y = self.player.y
+        target = None
+        dist_min = 1000000
+        for monster in monster_list:
+            m_x = monster.x
+            m_y = monster.y
+            dist = (p_x - m_x) ** 2 + (p_y - m_y) ** 2
+            if dist < dist_min:
+                dist_min = dist
+                target = monster
+        if target is None:
+            return
+        else:
+            velocity = (target.x - p_x, target.y - p_y)
+        x = self.player.x + self.player.width
+        y = self.player.y + self.player.height
+        Bullet((x, y), self.bullet, velocity, self.bullet_dmg, self.bullet_speed)
+
 
 class Bullet(Object):
-    def __init__(self, maker_pos, path):
+    def __init__(self, maker_pos, path, velocity, dmg, speed):
         super().__init__(maker_pos, path)
+        self.x += self.width / 2
+        self.y += self.height / 2
+        self.img = pygame.transform.scale(self.img, (30, 30))
+        self.rect = self.img.get_rect()
+        size = self.rect.size
+        self.width = size[0]
+        self.height = size[1]
+        self.x -= self.width / 2
+        self.y -= self.height / 2
+        self.rect.left = self.x
+        self.rect.top = self.y
+        self.dmg = dmg
+        self.v_x = velocity[0] * speed
+        self.v_y = velocity[1] * speed
+        player_bullet_list.append(self)
+
+    def move(self):
+        self.x += self.v_x
+        self.y += self.v_y
+        self.rect.left = self.x
+        self.rect.top = self.y
